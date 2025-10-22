93af21618ec6113f253fe2fdd67c8af4789003f7
AHasanSgt <ayeshahasan003@gmail.com>
2025-10-22 19:53:08 -0400

    test

--- api/main.py
+++ api/main.py
@@ -1,16 +1,5 @@
-# FastAPI is a Python library that 
-# allows us to 
-# - take in a request (typically sent from the client)
-# - send back a response
 from fastapi import FastAPI
-
-# CORS (Cross-Origin Resource Sharing)
-# allows us to restrict/enable which
-# client urls are allowed to call 
-# this backend code. 
-# CORS is part of the FastAPI library.
 from fastapi.middleware.cors import CORSMiddleware
-
 # Initialize the FastAPI application
 app = FastAPI(
     title="FastAPI Example",
@@ -26,30 +15,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 data = [
         {"name": "Alice", "age": 30, "city": "New York"},
         {"name": "Bob", "age": 24, "city": "London"},
         {"name": "Charlie", "age": 35, "city": "Paris"}
     ]
 
-
-@app.get("/")           #endpoint, or route, always starts with a forward slash
-def default_route():    #route handler function
+# define endpoints or routes
+@app.get("/")           
+def default_route():    
     """
     This is the default endpoint for this back-end.
     """
     return "You have reached the default route. Back-end server is listening..."
     
-@app.get("/example")  
-def get_example():    
+@app.get("/list")  
+def get_list():    
     """
     This endpoint returns a JSON object consisting of a simple message.
     """
-    return {"message": "Greetings! Brought to you by The Back-End Example Endpoint!"}
-
-
-# TO RUN:
-# 1. Put this code in api/main.py and deploy to Vercel
-# 2. Test by using your-vercel-backend-url/docs
-# 3. Later call from front-end using JavaScript fetch()
+    return message
